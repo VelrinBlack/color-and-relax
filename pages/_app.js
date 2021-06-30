9b803e8177222c42ae392699c720bd2cc3755801
Velrin Black <velrinblack@gmail.com>
2021-06-30 19:08:16 +0200

    Set up file tree

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
+import PropTypes from 'prop-types';
 
-function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
+
+App.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object,
+};
 
 export default MyApp;
