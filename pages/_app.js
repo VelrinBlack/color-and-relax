77e5166cb2b7a9a5d9d79b18694a774fa3d39c61
Velrin Black <velrinblack@gmail.com>
2021-07-03 08:06:00 +0200

    Set up styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'utils/styles/globalStyle';
+import theme from 'utils/styles/theme';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 App.propTypes = {
   Component: PropTypes.func,
---
+++ utils/styles/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  color: {
+    white: '#FFF',
+    purple: '#9A60C6',
+    red: '#FF1616',
+    darkGrey: '#2B2B2B',
+    lightGrey: '#424242',
+    lightGreyShadow: 'rgba(66, 66, 66, 50%)',
+  },
+  fontSize: {
+    XS: '14px',
+    S: '18px',
+    M: '36px',
+    L: '48px',
+    XL: '64px',
+  },
+  borderRadius: '3px',
+};
+
+export default theme;
