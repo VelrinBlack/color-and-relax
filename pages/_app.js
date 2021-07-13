18ce74f2891714064127a6f00b1c2e35fe349905
Velrin Black <velrinblack@gmail.com>
2021-07-13 11:57:55 +0200

    Add page title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'utils/styles/globalStyle';
 import theme from 'utils/styles/theme';
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </ThemeProvider>
+  <>
+    <Head>
+      <title>Color and Relax</title>
+      <link rel="icon" type="image/svg" href="/favicon.svg" />
+    </Head>
+
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 );
 
 App.propTypes = {
